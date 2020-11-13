cb7148d592fb9b7c7f8c36cf0e29db3f50bdcb49
Agguro <robertoaguasguerreiro@agguro.be>
2020-11-13 12:59:04 +0100

    Update scanexapp.php

--- ScanEx/php-script/scanexapp.php
+++ ScanEx/php-script/scanexapp.php
@@ -3,7 +3,7 @@
 // the call to this script must then be: https://yourserver/path-to-script/scanexapp.php?id=your-identifier
 // it is however not a secure way
 // Also create an upload directory and adjust permissons.
-if( isset($_GET['id']) && $_GET["id"]=="66102644933"){
+if( isset($_GET['id']) && $_GET["id"]=="IDENTIFIER"){
 	$file_path = "uploads/";
     $file_path = $file_path . basename( $_FILES['uploaded_file']['name']);
     if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $file_path)) {
